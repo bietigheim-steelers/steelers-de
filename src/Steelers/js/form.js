0b7475553b1b6c8ab0c87ad387f5e35657a60aaa
Dominik Sander <dominik.sander@duerrdental.com>
2024-10-22 15:18:12 +0200

    fix build issues

--- src/Steelers/js/form.js
+++ src/Steelers/js/form.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import FormApp from './FormApp.vue'
 import Vueform from '@vueform/vueform'
-import vueformConfig from './../../vueform.config'
+import vueformConfig from './../../../vueform.config'
 
 // Form
 const formApp = createApp(FormApp)
