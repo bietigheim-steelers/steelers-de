27cd5462d12d24612dff41cf3574a03fd2171633
Dominik Sander <dominik.sander@googlemail.com>
2024-11-05 11:40:40 +0100

    refactor game_id field to use options_callback for dynamic game selection

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -4,7 +4,7 @@ $GLOBALS['TL_DCA']['tl_news']['fields']['game_id'] = array(
   'label'                   => &$GLOBALS['TL_LANG']['tl_news']['game_id'],
   'exclude'                 => true,
   'inputType'               => 'select',
-  'foreignKey'              => 'tl_tilastot_client_games.id',
+  'options_callback'        => array('tl_news', 'getGamesForSelect'),
   'eval'                    => array('mandatory' => false, 'tl_class' => 'w50'),
   'sql'                     => "int(10) unsigned NOT NULL default '0'"
 );
@@ -14,3 +14,23 @@ $GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(
   '{game_legend},game_id;{title_legend}',
   $GLOBALS['TL_DCA']['tl_news']['palettes']['default']
 );
+
+class tl_news extends Backend
+{
+  public function getGamesForSelect()
+  {
+    $time = time();
+    $startDate = strtotime('-5 days', $time);
+    $endDate = strtotime('+5 days', $time);
+
+    $games = Database::getInstance()->prepare("SELECT id, hometeam, awayteam, gamedate FROM tl_tilastot_client_games WHERE gamedate BETWEEN ? AND ?")
+      ->execute($startDate, $endDate);
+
+    $options = array();
+    while ($games->next()) {
+      $options[$games->id] = sprintf('%s - %s vs. %s', date('d.m.Y', $games->gamedate), $games->hometeam, $games->awayteam);
+    }
+
+    return $options;
+  }
+}
