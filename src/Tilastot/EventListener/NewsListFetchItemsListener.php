c90ba898a9d9c71c432ea7b13249495ad92ec4ae
Dominik Sander <dominik.sander@duerrdental.com>
2024-12-18 15:41:11 +0100

    refactor: update NewsListFetchItemsListener to use attribute-based hook and simplify method signature

--- src/Tilastot/EventListener/NewsListFetchItemsListener.php
+++ src/Tilastot/EventListener/NewsListFetchItemsListener.php
@@ -1,26 +1,22 @@
 <?php
 namespace App\Tilastot\EventListener;
 
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
 use Contao\Module;
 use Contao\Date;
 use Contao\Input;
 use Contao\Config;
 use Contao\NewsModel;
 
+#[AsHook('newsListFetchItems')]
 class NewsListFetchItemsListener
 {
-public function videoportalFilter(array $newsArchives, ?bool $featuredOnly, int $limit, int $offset, Module $module)
+public function __invoke(array $newsArchives, ?bool $featuredOnly, int $limit, int $offset, Module $module)
 {
 
   // only use this hook for the videportal module (id 146)
   if ($module->id == '146') {
     
-    // Set the item from the auto_item parameter
-    if (!isset($_GET['items']) && isset($_GET['auto_item']) && Config::get('useAutoItem')) {
-      Input::setGet('items', Input::get('auto_item'));
-    }
-
-    var_dump(Input::get('items'));
 
     $time = Date::floorToMinute();
     $t = NewsModel::getTable();
