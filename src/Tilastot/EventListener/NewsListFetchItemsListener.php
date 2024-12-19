690a8ca128420dc8db872eb18caf0cf66cf45b59
Dominik Sander <dominik.sander@duerrdental.com>
2024-12-19 09:04:27 +0100

    feat: enhance NewsListFetchItemsListener to filter news by game_id and add video portal template

--- src/Tilastot/EventListener/NewsListFetchItemsListener.php
+++ src/Tilastot/EventListener/NewsListFetchItemsListener.php
@@ -15,24 +15,29 @@ class NewsListFetchItemsListener
 
     // only use this hook for the videportal module (id 146)
     if ($module->id == '146') {
+      $t = NewsModel::getTable();
       
       // Set the item from the auto_item parameter
       if (!isset($_GET['items']) && isset($_GET['auto_item']) && Config::get('useAutoItem')) {
         Input::setGet('items', Input::get('auto_item'));
       }
 
-      //var_dump(Input::get('items'));
+      $displayedNews = NewsModel::findBy('alias', Input::get('items'), ['limit' => 1]);
+      
+      if($displayedNews->game_id == '0') {
+        // return empty collection if no game is set
+        return NewsModel::findBy(array("$t.pid='nuller'"), null);
+      }
       
       $time = Date::floorToMinute();
-      $t = NewsModel::getTable();
-
-      $arrOptions['limit']  = $limit;
-      $arrOptions['offset'] = $offset;
+      $arrOptions['limit']  = 6;
+      $arrOptions['offset'] = 0;
 
       return NewsModel::findBy(
           array(
             "$t.pid IN(" . implode(',', array_map('\intval', $newsArchives)) . ")",
-            "$t.published='1' AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)"
+            "$t.published='1' AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)",
+            "$t.game_id = $displayedNews->game_id"
         ),
         null,
         $arrOptions
