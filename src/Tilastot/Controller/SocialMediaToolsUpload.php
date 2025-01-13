8b361b9189fed3d594274ccca0479d1c57f6a8b6
Dominik Sander <dominik.sander@duerrdental.com>
2025-01-13 14:18:14 +0100

    feat: add constructor and service configuration for SocialMediaToolsUpload controller

--- src/Tilastot/Controller/SocialMediaToolsUpload.php
+++ src/Tilastot/Controller/SocialMediaToolsUpload.php
@@ -6,6 +6,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Contao\CoreBundle\Framework\ContaoFramework;
 
 class SocialMediaToolsUpload {
+  private $smt_tennant_id;
+  private $smt_client_id;
+  private $smt_client_secret;
+  private $smt_team_id;
+  public function __construct(string $smt_tennant_id, string $smt_client_id, string $smt_client_secret, string $smt_team_id)
+  {
+    $this->smt_tennant_id = $smt_tennant_id;
+    $this->smt_client_id = $smt_client_id;
+    $this->smt_client_secret = $smt_client_secret;
+    $this->smt_team_id = $smt_team_id;
+  }
   public function upload(Request $request, ContaoFramework $framework): Response
   {
     
