e2bb52bf07d52f6713212a02d45fd7bf96507d72
Dominik Sander <dominik.sander@googlemail.com>
2025-02-08 15:41:22 +0100

    feat: add GeneratePageListener to track page access with Mixpanel

---
+++ src/Tilastot/EventListener/GeneratePageListener.php
@@ -0,0 +1,32 @@
+<?php
+// src/EventListener/GeneratePageListener.php
+namespace App\Tilastot\EventListener;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\PageRegular;
+use Contao\LayoutModel;
+use Contao\PageModel;
+use Mixpanel;
+
+#[AsHook('generatePage')]
+class GeneratePageListener
+{
+  public function __invoke(PageModel $pageModel, LayoutModel $layout, PageRegular $pageRegular, string $mixpanel_project_token): void
+  {
+
+    $mp = Mixpanel::getInstance($mixpanel_project_token);
+
+    $mp->identify($this->getUserId());
+    $mp->track("access", [
+      "path" => $_SERVER['REQUEST_URI'],
+      "referer" => $_SERVER['HTTP_REFERER'],
+      "agent" => $_SERVER['HTTP_USER_AGENT'],
+    ]);
+  }
+
+  private function getUserId()
+  {
+    $data = $_SERVER['REMOTE_ADDR'] . '-' . $_SERVER['HTTP_USER_AGENT'];
+    return hash('sha256', $data);
+  }
+}
