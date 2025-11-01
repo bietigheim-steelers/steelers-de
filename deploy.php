40557740f7290da78e1054b209ea9a71064da11f
Dominik Sander <dominik.sander@googlemail.com>
2025-11-01 14:35:23 +0100

    introduce deployer

---
+++ deploy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/contao.php';
+
+// Config
+set('repository', 'git@github.com:bietigheim-steelers/steelers-de.git');
+set('git_tty', true);
+
+set('bin/php', function () {
+  return '/usr/bin/php7.4';
+});
+set('bin/composer', function () {
+  return '{{bin/php}} ~/bin/composer.phar';
+});
+
+
+// Hosts
+host('steelers.de')
+  ->set('hostname', 'web01.steelers.de')
+  ->set('remote_user', 'scsteelers_deployer_website')
+  ->set('deploy_path', '~/web/2022-steelers-de');
+
+// Hooks
+after('deploy:failed', 'deploy:unlock');
+after('deploy:publish', 'deploy:cleanup');
