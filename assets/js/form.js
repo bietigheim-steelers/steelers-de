47a69e9d74ec8bf40633e4b3281a806f29563cc1
Dominik Sander <dominik.sander@googlemail.com>
2025-11-11 20:49:17 +0100

    js and css build fix

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,13 +1,11 @@
 import { createApp } from "vue";
-import FormApp from './FormApp.vue'
-import Vueform from '@vueform/vueform'
-import VueKonva from 'vue-konva';
-import vueformConfig from './../../../vueform.config'
+import FormApp from "./FormApp.vue";
+import Vueform from "@vueform/vueform";
+import VueKonva from "vue-konva";
+import vueformConfig from "../vueform.config";
 
 // Form
-const formApp = createApp(FormApp)
-formApp.use(Vueform, vueformConfig)
-formApp.use(VueKonva)
-formApp.mount('#formApp')
-
-
+const formApp = createApp(FormApp);
+formApp.use(Vueform, vueformConfig);
+formApp.use(VueKonva);
+formApp.mount("#formApp");
