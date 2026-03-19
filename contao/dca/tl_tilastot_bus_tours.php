93fd4838186bc9e80aba61f7abb438f770823c32
Dominik Sander <dominik.sander@duerrdental.com>
2026-03-19 08:33:12 +0100

    refactor: update delete confirmation handling to use null coalescing for improved robustness

--- contao/dca/tl_tilastot_bus_tours.php
+++ contao/dca/tl_tilastot_bus_tours.php
@@ -60,7 +60,7 @@ $GLOBALS['TL_DCA']['tl_tilastot_bus_tours'] = array(
                 'label'               => &$GLOBALS['TL_LANG']['tilastot_bus_tours']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.gif',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ),
             'toggle' => array(
                 'label'               => &$GLOBALS['TL_LANG']['tilastot_bus_tours']['toggle'],
--- contao/dca/tl_tilastot_client_rounds.php
+++ contao/dca/tl_tilastot_client_rounds.php
@@ -66,7 +66,7 @@ $GLOBALS['TL_DCA']['tl_tilastot_client_rounds'] = array
                 'label'               => &$GLOBALS['TL_LANG']['tl_tilastot_client_rounds']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.gif',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ),
             'show' => array
             (
