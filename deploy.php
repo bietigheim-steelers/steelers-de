adc5c916fac861f7291f0b112b200befd1f5238b
Dominik Sander <dominik.sander@googlemail.com>
2026-04-20 19:26:20 +0200

    fix: update Apache reload command to use force-reload for graceful restarts

--- deploy.php
+++ deploy.php
@@ -39,7 +39,7 @@ task('deploy:update_code', function () {
 
 desc('Reload Apache gracefully');
 task('apache:reload', function () {
-  run('sudo -n /usr/sbin/service apache2 reload');
+  run('sudo -n /usr/sbin/service apache2 force-reload');
 });
 
 add('shared_files', ['config/config.yml']);
