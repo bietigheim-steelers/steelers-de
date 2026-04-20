01c307bd0fcb45bc0b22270557651ec01864edb5
Dominik Sander <dominik.sander@googlemail.com>
2026-04-20 19:02:20 +0200

    fix: update Apache reload command to use systemctl

--- deploy.php
+++ deploy.php
@@ -39,7 +39,7 @@ task('deploy:update_code', function () {
 
 desc('Reload Apache gracefully');
 task('apache:reload', function () {
-  run('sudo service apache2 force-reload');
+  run('sudo /bin/systemctl reload apache2');
 });
 
 add('shared_files', ['config/config.yml']);
